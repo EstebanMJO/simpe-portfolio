12793925d8a114e21678d31e43d27caab1719a97
EstebanMJO <estebaan0@gmail.com>
2025-05-15 11:40:37 -0400

    update tests config

--- tests/test_portfolio.py
+++ tests/test_portfolio.py
@@ -6,3 +6,7 @@ def stocks_allocation():
     allocation = {'META': 0.4,
                   'APPL': 0.6}
     return allocation
+
+
+def test_allocation(stocks_allocation):
+    assert stocks_allocation['META'] == 0.4
