92a714a1a784a5a38261950c25422ca117c887fd
EstebanMJO <estebaan0@gmail.com>
2025-05-17 11:51:36 -0400

    add get stock instance without price

--- src/stock.py
+++ src/stock.py
@@ -11,27 +11,38 @@ class Stock:
     # This class variable holds the instances of the stocks
     _instances = {}
 
-    def __new__(cls, symbol: str, price: float):
+    def __new__(cls, symbol: str, price: float = None):
         '''
         This method is called every time a new instance of the class is created.
         It checks if an instance with the same symbol already exists.
         If it does, it returns the existing instance.
         '''
+        # Check if the symbol is valid
+        if not isinstance(symbol, str):
+            raise ValueError("Symbol must be a string")
+
         symbol = symbol.upper()  # Ensure the symbol is in uppercase
+
         # If the stock already exists, return the existing instance
         if symbol in cls._instances:
             stock = cls._instances[symbol]
-            # Update the price of the existing stock instance
-            if price != stock.price:
+
+            # If the price is provided update the existing instance.
+            if price is not None:
                 stock.current_price(price)
 
             print(f"Returning existing stock instance of {symbol}")
             return stock
 
+        # If the stock does not exist and price is None, raise an error
+        if price is None:
+            raise ValueError(
+                f'''Stock {symbol} not found. Price must be provided to create
+                a new instance.''')
+
         # If the stock does not exist, create a new instance and store it in
         # the class variable
         stock = super(Stock, cls).__new__(cls)
-        # Initialize the instance with the symbol and price
         stock._initialize(symbol, price)
         cls._instances[symbol] = stock
         return stock
@@ -41,6 +52,11 @@ class Stock:
         This method initializes the instance with the symbol and price.
         It is called only once when the instance is created.
         '''
+
+        # If the price is not valid, raise an error
+        if price <= 0:
+            raise ValueError("Price must be greater than zero")
+
         print(f"Creating new stock instance for {symbol} with price {price}")
         self.symbol = symbol.upper()  # Ensure the symbol is in uppercase
         self.price = price
