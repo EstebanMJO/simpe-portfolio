a406bcb4157e4879e3889a5af8e91a7beb04c41e
EstebanMJO <estebaan0@gmail.com>
2025-05-19 12:33:28 -0400

    add stock_qty_target and its update

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,4 +1,5 @@
-from stock import StockCollection
+from src.stock import StockCollection
+from src.utils import check_valid_allocation
 
 
 class Portfolio:
@@ -8,26 +9,29 @@ class Portfolio:
 
         self.name = name
         self.stocks_collection = stocks_collection
-        self.allocation_target = StockCollection()
+        self.stocks_qty_target = {}
+        self.allocation_target = {}
 
     def set_allocation_target(self,
-                              allocation_target: dict[str, float]) -> None:
-        """
-        This method sets the allocation target for the portfolio.
-        The allocation target is a dictionary with the stock symbol as the
-        key and the target quantity as the value.
-        """
-        portfolio_value = self.stocks_collection.get_value()
+                              allocation_target: dict[str: float]) -> None:
+        check_valid_allocation(allocation_target)
+        self.allocation_target = allocation_target
 
-        self.allocation_target = StockCollection(
-            stocks_qty=allocation_target,
-            target_value=portfolio_value)
+    def update_stocks_qty_target(self) -> None:
+        '''
+        This method sets the allocation target for the portfolio.
+        '''
+        portfolio_value = self.stocks_collection.get_value()
+        self.stocks_qty_target = StockCollection(
+            stocks_allocation=self.allocation_target,
+            total_value=portfolio_value)
 
     def get_allocation_deviation(self):
 
+        self.update_stocks_qty_target()
         deviation = {}
 
-        target_stocks = self.allocation_target.get_stocks_set()
+        target_stocks = self.stocks_qty_target.get_stocks_set()
         current_stocks = self.stocks_collection.get_stocks_set()
 
         modified_stocks = target_stocks.intersection(current_stocks)
@@ -35,12 +39,12 @@ class Portfolio:
         removed_stocks = current_stocks - target_stocks
 
         for stock in modified_stocks:
-            target_qty = self.allocation_target.stocks[stock]
+            target_qty = self.stocks_qty_target.stocks[stock]
             current_qty = self.stocks_collection.stocks[stock]
             deviation[stock] = current_qty - target_qty
 
         for stock in new_stocks:
-            target_qty = self.allocation_target.stocks[stock]
+            target_qty = self.stocks_qty_target.stocks[stock]
             deviation[stock] = -target_qty
 
         for stock in removed_stocks:
